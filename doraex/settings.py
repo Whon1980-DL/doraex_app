5d4d332dbe68f50b7a160d4a09861724ff51df89
Whon1980-DL <supitpongw@hotmail.com>
2025-03-29 12:19:09 +0000

    Chage debug setting to False

--- doraex/settings.py
+++ doraex/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)d0shh5v$t4-meo)mp2hgimz^xh1zoe$==f!)za$o18layg!h('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1', '.herokuapp.com'
