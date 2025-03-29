6ec8a4c2757c27f87a168332b34075f7774d6f0c
Whon1980-DL <supitpongw@hotmail.com>
2025-03-29 17:54:49 +0000

    Add product category filter to new view

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Gadget
+from .models import Gadget, Category
 
 
 # Create your views here.
@@ -8,3 +8,10 @@ class GadgetList(generic.ListView):
     queryset = Gadget.objects.all()
     template_name = "shop/index.html"
     paginate_by = 4
+
+
+def category(request, dl):
+    # Grab the category from the url
+    category = Category.objects.get(category_name=dl)
+    gadgets = Gadget.objects.filter(category=category)
+    return render(request, 'shop/category.html', {'gadgets': gadgets, 'category': category})
