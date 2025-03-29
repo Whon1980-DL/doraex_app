b3c199931abf9f00bca8c3765f495c931022998b
Whon1980-DL <supitpongw@hotmail.com>
2025-03-29 20:06:31 +0000

    add view for gadget view page

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GadgetList.as_view(), name='home'),
-    path('category/<str:dl>', views.category, name='category')
+    path('category/<str:dl>', views.category, name='category'),
+    path('<slug:slug>/', views.gadget_view, name='gadget_view'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Gadget, Category
 
 
 # Create your views here.
 class GadgetList(generic.ListView):
-    queryset = Gadget.objects.all()
+    queryset = Gadget.objects.filter(status=1)
     template_name = "shop/index.html"
     paginate_by = 4
 
@@ -13,5 +13,29 @@ class GadgetList(generic.ListView):
 def category(request, dl):
     # Grab the category from the url
     category = Category.objects.get(category_name=dl)
-    gadgets = Gadget.objects.filter(category=category)
+    gadgets = Gadget.objects.filter(category=category, status=1)
     return render(request, 'shop/category.html', {'gadgets': gadgets, 'category': category})
+
+
+def gadget_view(request, slug):
+    """
+    Display an individual :model:`shop.Gadget`.
+
+    **Context**
+
+    ``gadget``
+        An instance of :model:`shop.Gadget`.
+
+    **Template:**
+
+    :template:`shop/gadget_view.html`
+    """
+
+    queryset = Gadget.objects.filter(status=1)
+    gadget = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "shop/gadget_view.html",
+        {"gadget": gadget},
+    )
