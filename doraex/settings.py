8a97f67f830539028c7f32e0f21eb0720166633c
Whon1980-DL <supitpongw@hotmail.com>
2025-04-05 07:34:36 +0100

    Chnage debug setting to false reday for deployment

--- doraex/settings.py
+++ doraex/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-)d0shh5v$t4-meo)mp2hgimz^xh1zoe$==f!)za$o18layg!h('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1', '.herokuapp.com'
